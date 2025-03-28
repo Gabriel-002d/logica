eaf81b789d7e25f0d9709cb01703b83eaa51b86c
JOAO GABRIEL SOUSA DE OLIVEIRA <202503104972@alunos.unifanor.edu.br>
2025-03-28 10:41:47 -0300

    atualizacao das pastas

--- comdicionalsinples.py
+++ comdicionalsinples.py
@@ -1,4 +1,5 @@
 numero = 10
 if numero ==10:
     print("o numero informodo é igual a 10")
-    ilse:print("8")
+else:
+     print("8")
---
+++ operadoresAritmeticos.py
@@ -0,0 +1,35 @@
+# adição
+numero1 = 10
+numero2 = 20
+
+somw = numero1 + numero2
+print(somw) 
+
+# subtração
+
+subtraçao = numero1 -numero2
+print(subtraçao)
+
+# mutplicaçao
+
+mutplicasao = numero1 * numero2
+print(mutplicasao)
+
+# divisao
+divisao = numero1 // numero2
+print(divisao)
+
+# potencia
+
+potencia = numero1 ** numero2
+print(potencia)
+
+# modulo 
+
+modulo = numero1 % numero2
+print(modulo)
+
+# resto da divisao
+restodivisao = numero1 //numero2 
+print(restodivisao)
+
