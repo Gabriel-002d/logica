939a0b17d8f26ef50959954f2ae6a7d4dface6f5
JOAO GABRIEL SOUSA DE OLIVEIRA <202503104972@alunos.unifanor.edu.br>
2025-04-04 09:44:48 -0300

    atualizacao da pasta comdicionalsinples e criacao da pasta execicios

--- comdicionalsinples.py
+++ comdicionalsinples.py
@@ -1,5 +1,13 @@
-numero = 10
-if numero ==10:
-    print("o numero informodo é igual a 10")
-else:
-     print("8")
+#numero = 10
+#if numero ==10:
+    #print("o numero informodo é igual a 10")
+#else:
+     #print("8")
+
+#x = 5 
+#if x == 5:
+    #print(" x e igual a 5")
+
+numerodamatricula = 202495739479
+if numerodamatricula == 202495739479:
+    print(" numerodamatricula do fulano no e igual a 202495739479")
---
+++ exercicios.py
@@ -0,0 +1,8 @@
+soma = 11
+y = 1
+x = soma - 1
+if x == y:
+    print(" x e igual a y")
+
+else:
+  print(" x e maior que y")
